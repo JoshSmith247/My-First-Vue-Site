2f5cce73c453a61c1a9184e81fcb45bd7342a475
Josh S <54862229+JoshSmith247@users.noreply.github.com>
2025-05-20 16:32:26 -0500

    Basic Setup

---
+++ my-vue2-vuetify-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-toolbar color="indigo" dark>
+      <v-toolbar-title>Hello Vuetify 1.5</v-toolbar-title>
+    </v-toolbar>
+    <v-container>
+      <v-btn color="primary">Click me</v-btn>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ my-vue2-vuetify-app/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Vuetify)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
